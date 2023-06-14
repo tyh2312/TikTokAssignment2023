35d532882c43b919316b2e943d488673034223df
tyh2312 <31516640+tyh2312@users.noreply.github.com>
2023-06-15 00:02:37 +0800

    Create redis.go file as RedisClient

---
+++ rpc-server/script/redis.go
@@ -0,0 +1,26 @@
+import (
+    "context"
+    "encoding/json"
+    "github.com/redis/go-redis/v9"
+    "time"
+)
+
+type RedisClient struct {
+    cli *redis.Client
+}
+
+func (c *RedisClient) InitClient(ctx context.Context, address, password string) error {
+    r := redis.NewClient(&redis.Options{
+       Addr:     address,
+       Password: password, // no password set
+       DB:       0,        // use default DB
+    })
+
+    // test connection
+    if err := r.Ping(ctx).Err(); err != nil {
+       return err
+    }
+
+    c.cli = r
+    return nil
+}
